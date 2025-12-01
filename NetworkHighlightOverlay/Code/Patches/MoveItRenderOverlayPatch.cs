7a709d7c754a8cd33881cd039b6f0c346209218d
Sarah <lowpolyme@web.de>
2025-12-01 15:03:10 +0100

    added patch to handle move it rendering hiding our highlights
    MoveIt appears to use their own RenderOverlay and patch it to get our highlights to still show up while move it is active. According to https://github.com/Quboid/CS-MoveIt/blob/master/MoveIt/MoveItTool.cs 01/12/2026 this should be void RenderOverlay(RenderManager.CameraInfo cameraInfo). Patch will need to be updated when move it makes changes here

---
+++ NetworkHighlightOverlay/Code/Patches/MoveItRenderOverlayPatch.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Reflection;
+using HarmonyLib;
+using NetworkHighlightOverlay.Code.Core;
+using UnityEngine;
+
+namespace NetworkHighlightOverlay.Code.Patches
+{
+    /// <summary>
+    /// MoveIt appears to use their own RenderOverlay method, which unfortunately means we need to find
+    /// and patch it to get our highlights to still show up while move it is active.
+    /// according to https://github.com/Quboid/CS-MoveIt/blob/master/MoveIt/MoveItTool.cs 01/12/2026
+    /// this should be void RenderOverlay(RenderManager.CameraInfo cameraInfo)
+    /// will need to be updated when move it makes changes here
+    /// </summary>
+    [HarmonyPatch]
+    public static class MoveItRenderOverlayPatch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var toolType = AccessTools.TypeByName("MoveIt.MoveItTool");
+                if (toolType == null)
+                {
+                    return null;
+                }
+
+                return AccessTools.Method(
+                    toolType,
+                    "RenderOverlay",
+                    new[] { typeof(RenderManager.CameraInfo) }
+                );
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(
+                    $"[NetworkHighlightOverlay] Failed to locate MoveIt.MoveItTool.RenderOverlay: {e}"
+                );
+                return null;
+            }
+        }
+        
+        static void Postfix(RenderManager.CameraInfo cameraInfo)
+        {
+            try
+            {
+                Manager.Instance?.RenderIfActive(cameraInfo);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(
+                    $"[NetworkHighlightOverlay] Error in Move It RenderOverlay postfix: {e}"
+                );
+            }
+        }
+    }
+}
