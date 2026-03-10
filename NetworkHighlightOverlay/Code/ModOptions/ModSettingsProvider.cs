c1bd48a2216f12c52f3dd568c2de1d13030f4f0f
Sarah (LowPolyMe) <lowpolyme@web.de>
2026-03-10 15:01:59 +0100

    clean up unused file

--- NetworkHighlightOverlay/Code/ModOptions/ModSettingsProvider.cs
+++
@@ -1,8 +0,0 @@
-namespace NetworkHighlightOverlay.Code.ModOptions
-{
-    [System.Obsolete("Use ModCompositionRoot.Settings from the composition root.")]
-    public static class ModSettingsProvider
-    {
-        public static ModSettings GetSettings() => ModCompositionRoot.Settings;
-    }
-}
